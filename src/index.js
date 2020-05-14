e125e97751939ea4b908a3cefa5dcdeed0a9fa1d
Glen Pike <github@glenpike.co.uk>
2020-05-14 22:44:39 +0100

    Add babel-eslint for static properties and fix lint errors

--- src/index.js
+++ src/index.js
@@ -5,123 +5,130 @@ window.Tone = Tone
 import MicrobitUSB from './microbit-usb'
 import USBDeviceManager from './usb-device-manager'
 
+// Fix the vulnerabilities
+// Do a separate synth - defs?
+// Mapping input to notes and controls
+// Create 'wiring' handler
+
 //create a synth and connect it to the master output (your speakers)
 let synth = new Tone.Synth().toMaster()
 
-const deviceManager = new USBDeviceManager(MicrobitUSB);
+const deviceManager = new USBDeviceManager(MicrobitUSB)
 
 let running = false
 
-async function delay(ms) { new Promise(resolve => setTimeout(resolve, ms) ) }
+const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 // Move this, either to Microbit or 'middleware' kind of thing
 let serialText = ''
-function processSerialInput(serialData) {
-  let lines = []
-  let hex = ''
-  for(let i = 0; i < serialData.length; i++) {
-    const char = serialData[i]
-    if (char == "\n") {
-      if (serialText.length) {
-        lines.push(serialText.trim())
-      }
-      serialText = ''
-    } else {
-      serialText += char
+const processSerialInput = (serialData) => {
+    let lines = []
+    let hex = ''
+    for (let i = 0; i < serialData.length; i++) {
+        const char = serialData[i]
+        if (char == '\n') {
+            if (serialText.length) {
+                lines.push(serialText.trim())
+            }
+            serialText = ''
+        } else {
+            serialText += char
+        }
+        hex += `${serialData.charCodeAt([i]).toString(16)} `
     }
-    hex += `${serialData.charCodeAt([i]).toString(16)} `      
-  }
-  // console.log(`decoded '${hex}'\nSerial Str: '${serialData}'\nlines: ${lines}\nserialText: '${serialText}'`)
-  if(serialText.length > 256) {
-    serialText = ''
-  }
+    // console.log(`decoded '${hex}'\nSerial Str: '${serialData}'\nlines: ${lines}\nserialText: '${serialText}'`)
+    if (serialText.length > 256) {
+        serialText = ''
+    }
 
-  return lines;
+    return lines
 }
 
-function processDataPackets(lines) {
-  const packets = []
-  lines.forEach((line, index) => {
-    try {
-      let packet = JSON.parse(line)
-      packets.push(packet)
-    } catch(e) {
-      console.warn('Invalid packet @ ', index, line)
-    }
-  })
-  return packets;
+const processDataPackets = (lines) => {
+    const packets = []
+    lines.forEach((line, index) => {
+        try {
+            let packet = JSON.parse(line)
+            packets.push(packet)
+        } catch (e) {
+            console.warn('Invalid packet @ ', index, line)
+        }
+    })
+    return packets
 }
 
 let notes_1 = [36, 40, 43, 48, 52, 55, 60, 64, 67] // C E G arpeggios
 let notes_2 = [36, 38, 40, 41, 43, 45, 47, 48] //.map { |n| n + 24 }
 
 const mapNotes = (value, notes) => {
-  let input = Math.max(value, 0)
-  input = Math.min(input, 100)
-  let index = Math.floor(((input / 101) * notes.length))
-  return notes[index]
+    let input = Math.max(value, 0)
+    input = Math.min(input, 100)
+    let index = Math.floor((input / 101) * notes.length)
+    return notes[index]
 }
 
-function serialEventHandler(data) {
-  let lines = processSerialInput(data)
-  let packets = processDataPackets(lines)
-  if (packets.length) {
-    for(let i = 0;i < packets.length;i++) {
-      if(packets[i].n == 'pitch') {
-        let note = mapNotes(packets[i].v, notes_1)
-        // console.log('received pitch: ', note)
-        synth.triggerAttackRelease(Tone.Frequency(note, "midi") , '8n')
-      }
+const serialEventHandler = (data) => {
+    let lines = processSerialInput(data)
+    let packets = processDataPackets(lines)
+    if (packets.length) {
+        for (let i = 0; i < packets.length; i++) {
+            if (packets[i].n == 'pitch') {
+                let note = mapNotes(packets[i].v, notes_1)
+                // console.log('received pitch: ', note)
+                synth.triggerAttackRelease(Tone.Frequency(note, 'midi'), '8n')
+            }
+        }
     }
-  }
 }
 
-async function readUSBDevice() {
-  await Tone.start()
-  console.log('audio is ready')
-  
-  const microbit = await deviceManager.addDevice();
-  
-  if(!microbit) {
-    console.log('No microbit!')
-    return;
-  }
+const readUSBDevice = async () => {
+    await Tone.start()
+    console.log('audio is ready')
 
-  await microbit.connect()
+    const microbit = await deviceManager.addDevice()
 
-  console.log('Starting!')
-  running = true
-  
-  while(running) {
-    await microbit.readSerial()
-    await delay(50)
-  }
-  console.log('Stopping!')
-  microbit.disconnect()
+    if (!microbit) {
+        console.log('No microbit!')
+        return
+    }
+
+    await microbit.connect()
+
+    console.log('Starting!')
+    running = true
+
+    while (running) {
+        await microbit.readSerial()
+        await delay(50)
+    }
+    console.log('Stopping!')
+    microbit.disconnect()
 }
 
-function disconnectListener(data) {
-  console.log('microbit disconnected? ', data.device)
-  running = false;
+const disconnectListener = (data) => {
+    console.log('microbit disconnected? ', data.device)
+    running = false
 }
 
 const handlers = {
-  'serial': serialEventHandler,
-  'usbdevice::disconnect': disconnectListener
+    serial: serialEventHandler,
+    'usbdevice::disconnect': disconnectListener,
 }
 
-function messageHandler(event) {
-  let handler = handlers[event.data.type]
-  if(handler) {
-    handler(event.data.data)
-  }
+const messageHandler = (event) => {
+    let handler = handlers[event.data.type]
+    if (handler) {
+        handler(event.data.data)
+    }
 }
 
-(async () => {
-  window.addEventListener('message', messageHandler)
-  const go = document.querySelector('.js-button-go')
-  console.log('button ', go)
-  go.onclick = () => readUSBDevice()
-  const stop = document.querySelector('.js-button-stop')
-  stop.onclick = () => { running = false }
+;(async () => {
+    window.addEventListener('message', messageHandler)
+    const go = document.querySelector('.js-button-go')
+    console.log('button ', go)
+    go.onclick = () => readUSBDevice()
+    const stop = document.querySelector('.js-button-stop')
+    stop.onclick = () => {
+        running = false
+    }
 })()
