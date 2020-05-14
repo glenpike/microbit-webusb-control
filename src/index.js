d491258257175cc0652f944a156e3035bb57b870
Glen Pike <github@glenpike.co.uk>
2020-05-15 00:31:22 +0100

    Messing with Tone - controlling filter on a synth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ window.Tone = Tone
 import MicrobitUSB from './microbit-usb'
 import USBDeviceManager from './usb-device-manager'
 
-let synth = new Tone.Synth().toMaster()
+let filter = new Tone.Filter(200, "lowpass", -24)
+let synth = new Tone.Synth({
+  oscillator: {
+    type: 'fatsawtooth',
+  },
+})
+
+synth.chain(filter, Tone.Master)
+
+let seq = new Tone.Sequence(function(time, note){
+  synth.triggerAttackRelease(Tone.Frequency(note, 'midi'), '8n')
+}, [36, 40, [43, 43], 40/*, 48, 52, 55, 60, 64, 67*/], "8n").start(0)
 
 const deviceManager = new USBDeviceManager(MicrobitUSB)
 
@@ -51,14 +62,21 @@ const processDataPackets = (lines) => {
   return packets
 }
 
-let notes_1 = [36, 40, 43, 48, 52, 55, 60, 64, 67] // C E G arpeggios
-let notes_2 = [36, 38, 40, 41, 43, 45, 47, 48] //.map { |n| n + 24 }
+// let notes_1 = [36, 40, 43, 48, 52, 55, 60, 64, 67] // C E G arpeggios
+// let notes_2 = [36, 38, 40, 41, 43, 45, 47, 48] //.map { |n| n + 24 }
 
-const mapNotes = (value, notes) => {
+// const mapNotes = (value, notes) => {
+//   let input = Math.max(value, 0)
+//   input = Math.min(input, 100)
+//   let index = Math.floor((input / 101) * notes.length)
+//   return notes[index]
+// }
+
+const mapFilter = (value) => {
   let input = Math.max(value, 0)
   input = Math.min(input, 100)
-  let index = Math.floor((input / 101) * notes.length)
-  return notes[index]
+  let freq = (input / 100) * 2000 + 200
+  return freq
 }
 
 const serialEventHandler = (data) => {
@@ -66,10 +84,10 @@ const serialEventHandler = (data) => {
   let packets = processDataPackets(lines)
   if (packets.length) {
     for (let i = 0; i < packets.length; i++) {
-      if (packets[i].n == 'pitch') {
-        let note = mapNotes(packets[i].v, notes_1)
-        // console.log('received pitch: ', note)
-        synth.triggerAttackRelease(Tone.Frequency(note, 'midi'), '8n')
+      if (packets[i].n == 'roll') {
+        let freq = mapFilter(packets[i].v)
+        filter.frequency.value = freq
+        
       }
     }
   }
@@ -91,12 +109,14 @@ const readUSBDevice = async () => {
   console.log('Starting!')
   running = true
 
+  Tone.Transport.start()
   while (running) {
     await microbit.readSerial()
     await delay(50)
   }
   console.log('Stopping!')
   microbit.disable()
+  Tone.Transport.stop()
 }
 
 const disconnectListener = (data) => {
